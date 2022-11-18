e0be7c8ccb964ba45a0f8bf2bf5c593814505580
Ihor Khvorostyanyi <igor_gk@ukr.net>
2022-11-18 05:47:04 +0200

    Completed task02

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+	"Potatoes",
+	"Mushrooms",
+	"Garlic",
+	"Tomatos",
+	"Herbs",
+	"Condiments",
 ];
+
+const parentEl = document.querySelector("#ingredients");
+
+const newElements = ingredients.map((ingridientName) => {
+	const newChildEl = document.createElement("li");
+	newChildEl.classList.add("item");
+	newChildEl.textContent = ingridientName;
+	return newChildEl;
+});
+// console.log(newElements);
+parentEl.append(...newElements);

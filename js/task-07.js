50aadef1522a2fcb3c20fa5c4ddcc90350479061
Ihor Khvorostyanyi <igor_gk@ukr.net>
2022-11-22 02:15:31 +0200

    Task07 completed

---
+++ js/task-07.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const inputEl = document.querySelector("#font-size-control");
+const textEl = document.querySelector("#text");
+inputEl.addEventListener("input", (event) => {
+	textEl.style.fontSize = `${event.currentTarget.value}px`;
+});

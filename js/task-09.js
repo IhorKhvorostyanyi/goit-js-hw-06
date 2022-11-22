3e3114a2208c8ee90728a9b2c9074d7ec0839c75
Ihor Khvorostyanyi <igor_gk@ukr.net>
2022-11-22 03:23:13 +0200

    Task09 completed

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
+"use strict";
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+	return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyEl = document.body;
+const buttonEl = document.querySelector(".change-color");
+const colorValueEl = document.querySelector(".color");
+const handleClick = (event) => {
+	colorValueEl.textContent = getRandomHexColor();
+	bodyEl.style.backgroundColor = colorValueEl.textContent;
+};
+
+buttonEl.addEventListener("click", handleClick);

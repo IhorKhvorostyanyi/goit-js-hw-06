bdf0776d9f3b7c7753959015611a5ba0c16d319e
Ihor Khvorostyanyi <igor_gk@ukr.net>
2022-11-22 02:42:04 +0200

    Task08 completed

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+"use strict";
+const formEl = document.querySelector(".login-form");
+formEl.addEventListener("submit", handleSubmit);
 
+function handleSubmit(event) {
+	event.preventDefault();
+	const {
+		elements: { email, password },
+	} = event.currentTarget;
+
+	if (password.value === "" && email.value === "") {
+		console.log("Вы ничего не ввели!");
+		return;
+	} else if (password.value === "") {
+		console.log("Введите пароль!");
+		return;
+	} else if (email.value === "") {
+		console.log("Введите Эл.почту!");
+		return;
+	}
+
+	console.log(`Email: ${email.value}, Password: ${password.value}`);
+	event.currentTarget.reset();
+}
